444d86edac6cf0739c0c56ce8471324cf3aef250
Vasiliy Kupchinskiy <vasiliy.kupchinskiy@gmail.com>
2020-02-26 16:14:38 +0700

    Depl-150187 Support Tomcat 8.5 package manager installation

--- utils.sh
+++ utils.sh
@@ -456,7 +456,7 @@ function unpack_ps_war() {
     chown -R "$(whoami):$TOMCAT_GROUP" "$WEBAPP_PATH" || return 1
     (find "$WEBAPP_PATH" -type d -print0 | xargs -0 chmod g-w,g+x,o-r,o-w,o-x) || return 1
     (find "$WEBAPP_PATH" -type f -print0 | xargs -0 chmod g-w,o-r,o-w,o-x) || return 1
-    (find "$WEBAPP_PATH" -maxdepth 1 -type d -name 'css' -print0 | xargs -0 chmod g+w) || return 1
+    test -d "$WEBAPP_PATH/css" && (chmod -R g+w "$WEBAPP_PATH/css" || return 1)
 }
 
 function cleanup_tomcat() {
