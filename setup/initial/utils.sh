db1b0f1cdade7fb2eaf4391a4c5891640d71e3d9
Vasiliy Kupchinskiy <vasiliy.kupchinskiy@gmail.com>
2020-03-13 11:33:31 +0700

    110307 Upgrade installation config scripts for Java 11 Move env init to separate function

--- setup/initial/utils.sh
+++ setup/initial/utils.sh
@@ -7,22 +7,34 @@ function require_root_user() {
     fi
 }
 
-# Uses AWS_REGION variable
+function config_ec2_env() {
+    export EC2_ID
+    export EC2_REGION
+    export EC2_URL_INTERNAL
+    export EC2_IPV4
+
+    EC2_ID="$(ec2-metadata --instance-id | cut -d' ' -f2)"
+    EC2_REGION=$(ec2-metadata --availability-zone | cut -d ' ' -f2 | sed 's/[a-z]$//')
+    EC2_URL_INTERNAL=$(aws ec2 describe-tags --region "$EC2_REGION" --filters "Name=resource-id,Values=$EC2_ID" \
+        --filters "Name=key,Values=url-internal" | jq ".Tags[0].Value" | sed -r 's/\"//g')
+    EC2_IPV4="$(ec2-metadata --local-ipv4 | cut -d ' ' -f2)"
+}
+
+# Uses EC2_URL_INTERNAL, EC2_IPV4 variable
 function init_ec2_instance() {
     # Install Git, jq
     yum install -y git jq
 
-    EC2_ID="$(ec2-metadata --instance-id | cut -d' ' -f2)"
-    URL_INTERNAL=$(aws ec2 describe-tags --region "$AWS_REGION" --filters "Name=resource-id,Values=$EC2_ID" \
-        --filters "Name=key,Values=url-internal" | jq ".Tags[0].Value" | sed -r 's/\"//g')
-    ENV="${URL_INTERNAL#*.}"
-    IPV4="$(ec2-metadata --local-ipv4 | cut -d ' ' -f2)"
+    config_ec2_env
+
+    echo "Updating Route 53..."
+    update_route53
 
     # Update hostname
-    echo "HOSTNAME=$URL_INTERNAL" >>"/etc/sysconfig/network"
-    echo -n "$IPV4 ${URL_INTERNAL}.ov.internal" >>"/etc/hosts"
-    echo " $URL_INTERNAL" | sed 's/\./-/g' >>"/etc/hosts"
+    echo "HOSTNAME=$EC2_URL_INTERNAL" >>"/etc/sysconfig/network"
+    echo -n "$EC2_IPV4 ${EC2_URL_INTERNAL}.ov.internal" >>"/etc/hosts"
+    echo " $EC2_URL_INTERNAL" | sed 's/\./-/g' >>"/etc/hosts"
 
     # Update nickname for terms
-    echo "export NICKNAME=$URL_INTERNAL" | sed 's/\./-/g' >"/etc/profile.d/prompt.sh"
+    echo "export NICKNAME=$EC2_URL_INTERNAL" | sed 's/\./-/g' >"/etc/profile.d/prompt.sh"
 }
