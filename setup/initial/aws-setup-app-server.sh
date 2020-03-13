469118b6af9cf82cf554577b43167fe9a72fec25
Vasiliy Kupchinskiy <vasiliy.kupchinskiy@gmail.com>
2020-03-13 17:38:15 +0700

    110307 Upgrade installation config scripts for Java 11 Refactor

--- setup/initial/aws-setup-app-server.sh
+++ setup/initial/aws-setup-app-server.sh
@@ -1,34 +1,36 @@
 #!/bin/bash
 
-if [ "$#" -ne 4 ]; then
+function usage() {
     echo "### This script will automatically set up the services needed to run on the app server (based on AWS) ###"
-    echo "Usage: $(basename "$0") <website> <website version> <database user (owner schema)> <database host:port:sid>"
     echo " "
-    echo "Example: $(basename "$0") test.onevizion.com 20.3.0 prod01 db01.template.ov.internal:1521:P1"
+    echo "Usage: $(basename "$0") <config file>"
+    echo " "
+    echo "Example: $(basename "$0") aws-setup.conf"
+}
+
+if [ "$#" -ne 1 ]; then
+    usage
     exit 1
 fi
 
 # shellcheck source=utils.sh
 . "$(dirname "$0")/utils.sh"
 
-# shellcheck source=aws-utils.sh
-. "$(dirname "$0")/aws-utils.sh"
-
 require_root_user
+
+CONFIG_FILE="$1"
+
+# shellcheck source=aws-setup.conf.template
+. "$CONFIG_FILE"
 
 set -o errexit
 set -o pipefail
 set -o nounset
 
-WEBSITE=$1
-VERSION=$2
-DB_USER=$3
-DB_URL=$4
+init_ec2_instance
 
 # Install Java 11 (Correto)
 amazon-linux-extras install -y java-openjdk11
-
-init_ec2_instance
 
 yum install -y python3
 
@@ -45,8 +47,4 @@ python3 -m pip install pandas
 python3 -m pip install boto3
 
 # Run setup-app-server.sh
-"$(dirname "$0")/setup_app_server.sh" "$WEBSITE" "$VERSION" "$DB_USER" "$DB_URL" "$MAIN_PASSWORD" "$USER_PASSWORD" \
-    "$RPT_PASSWORD"
-
-# Update internal DNS
-"$SCRIPTS_DIR/update-route53.sh"
+"$(dirname "$0")/setup-app-server.sh" "$CONFIG_FILE"
