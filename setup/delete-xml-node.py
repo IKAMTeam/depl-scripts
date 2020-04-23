8d6e34768fd103834a75a2d18da8325e330c1177
Vasiliy Kupchinskiy <vasiliy.kupchinskiy@gmail.com>
2020-04-23 12:22:00 +0700

    110307 Upgrade installation config scripts for Java 11 Fix python script for delete xml node

--- setup/delete-xml-node.py
+++ setup/delete-xml-node.py
@@ -3,13 +3,13 @@
 import sys
 import xml.etree.ElementTree as ElementTree
 
-if len(sys.argv) < 3:
-    print('Usage: delete-xml-node.py <in-out-file> <xpath of node(s) which should be deleted>')
+if len(sys.argv) < 4:
+    print('Usage: delete-xml-node.py <in-out-file> <xpath of parent node(s)> <xpath of node(s) which should be deleted>')
     exit(1)
 
 inFile = sys.argv[1]
-childInFile = sys.argv[2]
-xpath = sys.argv[3]
+xpath1 = sys.argv[2]
+xpath2 = sys.argv[3]
 
 
 class CommentedTreeBuilder(ElementTree.TreeBuilder):
@@ -19,29 +19,12 @@ class CommentedTreeBuilder(ElementTree.TreeBuilder):
         self.end(ElementTree.Comment)
 
 
-def indent(elem, level=0):
-    i = "\n" + level * "    "
-    if len(elem):
-        if not elem.text or not elem.text.strip():
-            elem.text = i + "    "
-        if not elem.tail or not elem.tail.strip():
-            elem.tail = i
-        for elem in elem:
-            indent(elem, level + 1)
-        if not elem.tail or not elem.tail.strip():
-            elem.tail = i
-    else:
-        if level and (not elem.tail or not elem.tail.strip()):
-            elem.tail = i
-
-
 parser = ElementTree.XMLParser(target=CommentedTreeBuilder())
 tree = ElementTree.parse(inFile, parser)
 root = tree.getroot()
 
-for node in root.findall(xpath):
-    root.remove(node)
-
-indent(root)
+for parentNode in root.findall(xpath1):
+    for childNode in parentNode.findall(xpath2):
+        parentNode.remove(childNode)
 
 tree.write(inFile, encoding='utf-8', xml_declaration=True)
