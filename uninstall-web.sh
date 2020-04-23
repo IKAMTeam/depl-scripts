10885f1cc7b897a8790cc352944d0f9adfe3b6b0
Vasiliy Kupchinskiy <vasiliy.kupchinskiy@gmail.com>
2020-04-23 12:26:51 +0700

    110307 Upgrade installation config scripts for Java 11 Add debug lines

--- uninstall-web.sh
+++ uninstall-web.sh
@@ -17,9 +17,6 @@ require_root_user
 
 WEBSITE=$1
 
-SERVER_XML_FILE="$TOMCAT_PATH/conf/server.xml"
-CONTEXT_XML_FILE="$TOMCAT_PATH/conf/Catalina/$WEBSITE/ROOT.xml"
-
 START_TOMCAT=0
 if is_daemon_running "$TOMCAT_SERVICE"; then
     echo "Stopping Tomcat..."
@@ -27,6 +24,9 @@ if is_daemon_running "$TOMCAT_SERVICE"; then
 
     START_TOMCAT=1
 fi
+
+SERVER_XML_FILE="$TOMCAT_PATH/conf/server.xml"
+CONTEXT_XML_FILE="$TOMCAT_PATH/conf/Catalina/$WEBSITE/ROOT.xml"
 
 ENGINE_XPATH="Service/Engine[@name=\"Catalina\"]"
 HOST_XPATH="Host[@name=\"$WEBSITE\"]"
@@ -42,15 +42,17 @@ PROPERTIES_PATH="$TOMCAT_PATH/$WEBSITE"
 CATALINA_HOME_VAR='${catalina.home}'
 DOC_BASE_PATH="${DOC_BASE_PATH//$CATALINA_HOME_VAR/$TOMCAT_PATH}"
 
+echo "$APP_BASE_PATH"
+echo "$DOC_BASE_PATH"
+echo "$CONTEXT_PATH"
+echo "$PROPERTIES_PATH"
+
 (test -d "$APP_BASE_PATH" && rm -rf "$APP_BASE_PATH") || exit 1
 (test -d "$DOC_BASE_PATH" && rm -rf "$DOC_BASE_PATH") || exit 1
 (test -d "$CONTEXT_PATH" && rm -rf "$CONTEXT_PATH") || exit 1
 (test -d "$PROPERTIES_PATH" && rm -rf "$PROPERTIES_PATH") || exit 1
 
-echo "$APP_BASE_PATH"
-echo "$DOC_BASE_PATH"
-echo "$CONTEXT_PATH"
-echo "$PROPERTIES_PATH"
+echo debug
 
 "$(dirname "$0")/setup/delete-xml-node.py" "$SERVER_XML_FILE" "$ENGINE_XPATH" "$HOST_XPATH" || exit 1
 
