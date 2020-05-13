8f4cf962d1095b951653bcf8f17a9cf566adb67d
Vasiliy Kupchinskiy <vasiliy.kupchinskiy@gmail.com>
2020-05-13 15:06:32 +0700

    Other-159142 Add iptables rules for redirect traffic from 80 to 8080 ports

--- setup/initial/aws-setup-web-server.sh
+++ setup/initial/aws-setup-web-server.sh
@@ -31,5 +31,12 @@ init_ec2_instance
 # Install Tomcat (includes Java 11 (Correto))
 amazon-linux-extras install -y tomcat8.5
 
+# Temporary workaround to support legacy AWS ELB Health Check configuration
+yum install -y iptables-services
+systemctl enable iptables
+
+iptables -t nat -I PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 8080
+service iptables save
+
 # Run setup-web-server.sh
 "$(dirname "$0")/setup-web-server.sh" "-" <<< "$CONFIG_DATA"
