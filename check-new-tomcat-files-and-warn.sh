c86bc8dbb78f06d1c53c89aa81a78755812da386
Vasiliy Kupchinskiy <vasiliy.kupchinskiy@gmail.com>
2021-03-25 20:33:02 +0700

    Depl-175542 Handle *.rpmnew files after Tomcat upgrade

--- check-new-tomcat-files-and-warn.sh
+++ check-new-tomcat-files-and-warn.sh
@@ -8,6 +8,6 @@ require_root_user
 
 RPMNEW_FILES=$(find -L "$TOMCAT_PATH" -name '*.rpmnew' -type f)
 if [ -n "$RPMNEW_FILES" ]; then
-    echo "WARNING!!! the following files were modified during upgrade and should be reviewed and merged with original files containing OneVizion modification:"
+    echo "WARNING!!! the following files were modified during upgrade and should be reviewed and merged with original files containing OneVizion modifications:"
     echo "$RPMNEW_FILES"
 fi
