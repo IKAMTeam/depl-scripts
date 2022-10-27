67825167cd5048a8e4d0e0d0a3db9221faa8b11b
Vasiliy Kupchinskiy <vasiliy.kupchinskiy@gmail.com>
2022-10-27 11:26:59 +0500

    Depl-218574 Synchronize monitoring config with trackor.onevizion.com

--- setup/templates/python-services/monitoring-refresh-config/xmlhelper.py
+++ setup/templates/python-services/monitoring-refresh-config/xmlhelper.py
@@ -8,8 +8,18 @@ def prettify_xml(elem):
     return minidom_tree.toprettyxml(indent="	")
 
 
-# TODO: input-output examples
 def dict_to_xml_element(input_dict, output_tag_name):
+    """Convert dictionary to XML tag
+
+    Args:
+        input_dict (dict): Input dictionary
+        output_tag_name (str): XML tag name in result XML element
+
+    Returns:
+        ElementTree.Element: Created XML element
+
+    """
+
     xml_elem = ElementTree.Element(output_tag_name)
     for key, value in input_dict.items():
         attr = ElementTree.SubElement(xml_elem, key)
@@ -17,8 +27,19 @@ def dict_to_xml_element(input_dict, output_tag_name):
     return xml_elem
 
 
-# TODO: input-output examples
 def rows_to_xml_elements(rows, root_tag_name, child_tag_name):
+    """Convert list of dictionaries to XML element
+
+    Args:
+        rows (list[dict]): Input list of dictionaries
+        root_tag_name (str): XML tag name in result root XML element
+        child_tag_name (str): XML tag name in result children XML elements
+
+    Returns:
+        ElementTree.Element: Created XML element
+
+    """
+
     root_elem = ElementTree.Element(root_tag_name)
     for row in rows:
         root_elem.append(dict_to_xml_element(row, child_tag_name))
