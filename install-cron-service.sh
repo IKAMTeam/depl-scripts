acd676016c872e621b858ee544b234d5dd6d7a99
Vasiliy Kupchinskiy <vasiliy.kupchinskiy@gmail.com>
2022-10-31 14:51:17 +0500

    Depl-218574 Synchronize monitoring config with trackor.onevizion.com

--- install-cron-service.sh
+++ install-cron-service.sh
@@ -102,6 +102,7 @@ function install_python_and_dependencies() {
     PYTHON_REQUIREMENTS_FILE="$(get_python_service_requirements_file "$ARTIFACT")"
     if [ -f "$PYTHON_REQUIREMENTS_FILE" ]; then
         echo "Installing Python dependencies (to service user)..."
+        chmod +r "$PYTHON_REQUIREMENTS_FILE"
         sudo -u "$SERVICE_UN" python3 -m pip install -r "$PYTHON_REQUIREMENTS_FILE" --upgrade --user || return 1
     fi
 }
