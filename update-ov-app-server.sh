cf8a6ba985f46553f923be2e664b96456439fb95
Vasiliy Kupchinskiy <vasiliy.kupchinskiy@gmail.com>
2024-02-08 23:51:04 +0500

    Depl-230218 New script to upgrade all installed serives by website name Depl-230218-151936 [DEV] New script to upgrade all installed serives by website name

--- update-ov-app-server.sh
+++ update-ov-app-server.sh
@@ -18,8 +18,9 @@ require_root_user
 MATCH_WEBSITE=$1
 NEW_VERSION=$2
 
+FORCE_UPDATE_ARG=""
 if [ -n "$3" ] && { [ "$3" == "-f" ] || [ "$3" == "--force" ]; }; then
-    FORCE_UPDATE="1"
+    FORCE_UPDATE_ARG="--force"
 fi
 
 mapfile -t ALL_SERVICE_NAMES < <("$(dirname "$0")/list-services.sh" --short-format)
@@ -36,7 +37,7 @@ for SERVICE_NAME in "${ALL_SERVICE_NAMES[@]}"; do
     # shellcheck disable=SC2153
     SERVICE_PATH="$SERVICES_PATH/$SERVICE_NAME"
 
-    if ! is_snapshot_version "$NEW_VERSION" && [ "$FORCE_UPDATE" != "1" ]; then
+    if ! is_snapshot_version "$NEW_VERSION" && [ "$FORCE_UPDATE_ARG" != "--force" ]; then
         ARTIFACT_JAR="${ARTIFACT}.jar"
         ARTIFACT_VERSION="$(extract_and_read_artifact_version "$SERVICE_PATH/$ARTIFACT_JAR")"
 
@@ -57,30 +58,5 @@ fi
 
 for SERVICE_NAME in "${SERVICE_NAMES_TO_UPDATE[@]}"; do
     ARTIFACT="$(get_artifact_name "$SERVICE_NAME")"
-    # shellcheck disable=SC2153
-    SERVICE_PATH="$SERVICES_PATH/$SERVICE_NAME"
-
-    echo "Deploying [$ARTIFACT $NEW_VERSION] at"
-    printf '%s\n' "$SERVICE_PATH"
-
-    # Will export next variables: REPORT_EXEC_DOWNLOAD_PATH, EXPORT_EXEC_DOWNLOAD_PATH, DOWNLOAD_PATH
-    download_service_artifacts "$ARTIFACT" "$NEW_VERSION" || exit 1
-
-    echo "Updating [$SERVICE_NAME] at [$SERVICE_PATH]..."
-
-    if is_daemon_running "$SERVICE_NAME"; then
-        echo "Stopping [$SERVICE_NAME]..."
-        systemctl stop "$SERVICE_NAME"
-    fi
-
-    copy_service_artifacts "$ARTIFACT" || exit 1
-
-    if is_daemon_installed "$SERVICE_NAME"; then
-        extract_launcher_script "$ARTIFACT" || exit 1
-
-        echo "Starting [$SERVICE_NAME]..."
-        systemctl start "$SERVICE_NAME" || exit $?
-    elif is_cron_installed "$SERVICE_NAME"; then
-        extract_cron_launcher_script "$ARTIFACT" || exit 1
-    fi
+    "$(dirname "$0")/update-ov.sh" "$MATCH_WEBSITE" "$ARTIFACT" "$NEW_VERSION" "$FORCE_UPDATE_ARG" || exit 1
 done
