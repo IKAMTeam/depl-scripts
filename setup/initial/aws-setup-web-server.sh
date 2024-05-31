63c72a8cd4ab0fa8db60cf699c0be31b1e020679
Vasiliy Kupchinskiy <vasiliy.kupchinskiy@gmail.com>
2024-05-31 12:16:01 +0500

    Depl-229516 Upgrade depl-scripts to support AL2023 Depl-229516-165440 [DEV] Install Tomcat 10 and Java 21

--- setup/initial/aws-setup-web-server.sh
+++ setup/initial/aws-setup-web-server.sh
@@ -47,7 +47,8 @@ service iptables save
 yum install -y python3-dnf-plugin-post-transaction-actions
 
 # Create yum post-action to restore permissions after Tomcat package install/update
-(< "$SCRIPTS_PATH/setup/templates/yum/post-actions/tomcat.action" envsubst | tee "/etc/dnf/plugins/post-transaction-actions.d/tomcat.action") >/dev/null
+# TODO: Uncomment after Tomcat 10 will available in yum
+# (< "$SCRIPTS_PATH/setup/templates/yum/post-actions/tomcat.action" envsubst | tee "/etc/dnf/plugins/post-transaction-actions.d/tomcat.action") >/dev/null
 
 # Run setup-web-server.sh
 "$(dirname "$0")/setup-web-server.sh" "-" <<< "$CONFIG_DATA"
