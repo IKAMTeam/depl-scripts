557c4afb94b6a62e09049555ff9d97647be559a5
Vasiliy Kupchinsky <vasiliy.kupchinskiy@onevizion.com>
2026-06-24 11:51:37 +0500

    Depl-272373 Store local maven repository in static directory Depl-272373-243106 [DEV] Store local maven repository in static directory

--- utils.sh
+++ utils.sh
@@ -215,9 +215,6 @@ function download_artifact() {
                 rm -rf "$MVN_CACHE_DIR/com/onevizion"
                 echo "Maven cache size after download (without OneVizion artifacts): $(du -sh "$MVN_CACHE_DIR" 2>/dev/null | cut -f1)"
 
-                # Close lock descriptor
-                exec {LOCK_FD}>&-
-
                 return 0
             else
                 echo "Unable to copy [$ARTIFACT_PATH] to [$DOWNLOAD_PATH]"
