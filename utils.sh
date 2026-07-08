83fcc59ab87113de5d369dd7fc3abcdeedca7a4f
Vasiliy Kupchinsky <vasiliy.kupchinskiy@onevizion.com>
2026-07-08 11:49:05 +0500

    Depl-272994 Install Python's UV in initial scripts Depl-272994-244132 [DEV] Install Python's UV in initial scripts

--- utils.sh
+++ utils.sh
@@ -764,11 +764,11 @@ function update_uv() {
 
     echo "Updating uv for user [$USER]"
 
-    sudo -H -u "$USER" bash -lc 'uv self update' || return 1
-    sudo -H -u "$USER" bash -lc 'uv self version' || return 1
+    sudo -H -u "$USER" bash -lc 'cd ~ && uv self update' || return 1
+    sudo -H -u "$USER" bash -lc 'cd ~ && uv self version' || return 1
 
-    echo "uv cache directory: $(sudo -H -u "$USER" bash -lc 'uv cache dir')"
-    echo "uv cache size: $(sudo -H -u "$USER" bash -lc 'uv cache size -H')"
+    echo "uv cache directory: $(sudo -H -u "$USER" bash -lc 'cd ~ && uv cache dir')"
+    echo "uv cache size: $(sudo -H -u "$USER" bash -lc 'cd ~ && uv cache size -H')"
 }
 
 function install_uv() {
